b8a6a15caed4a8de0124c6fd4e059826dafcdb57
Daniel Zvara <dan.zvara@gmail.com>
2017-06-01 18:54:16 +0100

    client: added react skeleton for client side
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

---
+++ client/src/components/Button.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+function MyButton() {
+  return <button
+            title="CoolButton"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          >Button</button>;
+}
+
+export default MyButton;
+
