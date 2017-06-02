44361c0079ffc6b92367c9cddb46d6a2a78cc7c3
Peter Kovary <peter.kovary15@imperial.ac.uk>
2017-06-02 01:58:33 +0100

    [Client] Create Header and Footer common skeletons

---
+++ client/src/components/common/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const style = {
+
+};
+const Footer = () => {
+  return (
+    <footer className="footer" style={style}>
+    </footer>
+  )
+};
+export default Footer;
---
+++ client/src/components/common/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const style = {
+
+};
+const Header = () => {
+  return (
+    <header className="footer" style={style}>
+    </header>
+  )
+};
+export default Header;
