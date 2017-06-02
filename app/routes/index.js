15a3a40d9d7e423f510e3bcfb0f6a9631afff942
Peter Kovary <peter.kovary15@imperial.ac.uk>
2017-06-02 01:17:43 +0100

    Style fixes

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,20 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Express'});
 });
 
 router.post('/runpy', function (req, res) {
-    const code = req.body.code;
-    const resultStream = new stream.Writable();
-    resultStream._write = function (chunk, encoding, done) {
-        console.log(chunk.toString());
-        done();
-    };
+  const code = req.body.code;
+  const resultStream = new stream.Writable();
+  resultStream._write = function (chunk, encoding, done) {
+    console.log(chunk.toString());
+    done();
+  };
 
-    docker.run('pydock', ['bash', '-c', 'python -c \"' + code + '\"'], resultStream, function (err, dt, container) {
-    });
+  docker.run('pydock', ['bash', '-c', 'python -c \"' + code + '\"'], resultStream, function (err, dt, container) {
+  });
 });
 
 module.exports = router;
