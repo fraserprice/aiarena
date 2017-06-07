b20c1ba044ea68e6343176ea4dee5c7a312513b7
Daniel Zvara <dan.zvara@gmail.com>
2017-06-07 20:04:07 +0100

    [App]:socket: add function for emitting messages
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

--- app/socket/socket.js
+++ app/socket/socket.js
@@ -1,10 +1,27 @@
 'use strict'
 
 var io;
+var clients = [];
 
 function init(server) {
   io = require('socket.io')(server);
-  io.on('connection', (socket) => {console.log("user connected");});
+  io.on('connection', connectUser);
 }
 
-module.exports = init;
+function connectUser(socket) {
+  console.log("userID: " + socket.id + " connected");
+  clients.push(socket);
+}
+
+function pushToClient(clientID, eventType, data) {
+  for (var i = 0; i < clients.length; i++) {
+    if (clients[i].id == clientID) {
+      var socket = clients[i];
+      console.log("pushing message: " + data + ", to clientID: " + clientID);
+      socket.emit(eventType, data);
+      break;
+    }
+  }
+}
+
+module.exports = { init, pushToClient }
