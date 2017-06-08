41c03c3cf23ddbdf02a636e89ef556774c541100
Daniel Zvara <dan.zvara@gmail.com>
2017-06-08 22:45:31 +0100

    [App]: remove session modules
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

--- app/app.js
+++ app/app.js
@@ -34,9 +34,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
-app.use(passport.session());
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
