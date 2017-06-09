9ef7d5870d590c292f85d7ac1d31c793080b3e61
Daniel Zvara <dan.zvara@gmail.com>
2017-06-09 05:26:24 +0100

    [App]: mongo callback
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

--- app/app.js
+++ app/app.js
@@ -21,7 +21,7 @@ const app = express();
 const dbURL = 'mongodb://aiarena:mongo@ds019966.mlab.com:19966/heroku_ll75kc63'
 
 mongoose.Promise = global.Promise;
-mongoose.connect(dbURL);
+mongoose.connect(dbURL, (err) => {console.log(err);});
 require('./config/passport');
 
 app.use(cors());
